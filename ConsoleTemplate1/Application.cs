7dc2b137bc8208a434e2675a3ce3b9fd4ab455dd
teenak <teenak77@gmail.com>
2019-10-24 18:37:30 +0900

    moved Application to separated file

---
+++ ConsoleTemplate1/Application.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTemplate1
+{
+    public class Application
+    {
+        ILogger _logger;
+        IConfiguration _config;
+
+        public Application(ILogger<Application> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public async Task Run()
+        {
+            try
+            {
+
+                var name = _config.GetValue<string>("myoptions:name");
+
+                _logger.LogError($"This is a console application ");
+                _logger.LogInformation($"This is a console application for {name}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+            }
+        }
+    }
+}
--- ConsoleTemplate1/Program.cs
+++ ConsoleTemplate1/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using System;
 using System.IO;
 using System.Threading.Tasks;
@@ -41,33 +40,5 @@ namespace ConsoleTemplate1
                 .Build();
         }
 
-
-        public class Application
-        {
-            ILogger _logger;
-            IConfiguration _config;
-
-            public Application(ILogger<Application> logger, IConfiguration config)
-            {
-                _logger = logger;
-                _config = config;
-            }
-
-            public async Task Run()
-            {
-                try
-                {
-
-                    var name = _config.GetValue<string>("myoptions:name");
-
-                    _logger.LogError($"This is a console application ");
-                    _logger.LogInformation($"This is a console application for {name}");
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex.ToString());
-                }
-            }
-        }
     }
 }
